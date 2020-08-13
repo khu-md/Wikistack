caca645b1189f2d1dd130a310b34e9590336a62c
Kevin Hu <hukxchen+git@gmail.com>
2020-08-13 15:27:15 -0500

    Created User and Page models and placed db.sync
    Read about Use of promises on MDN
    Also learned about ENUM datatype for Postgres

--- index.js
+++ index.js
@@ -5,31 +5,46 @@ const PORT = 1234;
 
 // Require some middleware
 const morgan = require('morgan');
-const path = require("path")
+const path = require('path');
 
 // Require some views
 const main = require('./views/main');
 
 // Require database
-
-const { db } = require("./models");
+const { db } = require('./models');
 
 // MIDDLEWARE
-app.use('/public', express.static( path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
-
-// DATABASE
-db.authenticate()
-  .then(() => {
-    console.log('connected to the database');
-  })
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send(main());
 });
 
+// DATABASE
+db.authenticate().then(() => {
+  console.log('Connected to the database');
+});
+
+( async () => {
+  try {
+    await db.sync();
+    console.log("Database tables created!");
+    app.listen(PORT, () => {
+      console.log('Listening on --> ', PORT);
+    });
+  } catch (error) {
+    console.log("Something went wrong -->", error.message);
+  }
+})();
+
 // LISTEN
-app.listen(PORT, () => { console.log("Listening on --> ", PORT)});
-// app.listen(PORT);
+
+// (async () => {
+//   try
+//   await db.sync();
+//   console.log()
+// })();
+
