8e3dd19ec43f87f8e3b94794a592c97d7e3e3abb
Kevin Hu <hukxchen+git@gmail.com>
2020-08-13 12:00:52 -0500

    We hooked up our main view and started a basic stylesheet.
    Learned about layout and module.exports

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+// Set up our express app
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
 const PORT = 1234;
 
+// Require some middleware
+const morgan = require('morgan');
+
+// Require some views
+const main = require('./views/main');
+
+// MIDDLEWARE
 app.use(express.static('FOLDER PATH')); // update with folder path
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 
+// ROUTES
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(main());
 });
 
+// LISTEN
 app.listen(PORT, () => { console.log("Listening on --> ", PORT)});
 // app.listen(PORT);
