bb32b17b245386b2cfa0b4adea8cc48095bfc1c5
Kevin Hu <hukxchen+git@gmail.com>
2020-08-13 11:36:27 -0500

    Use nodemon.
    Define a port 1234 to be listening on.
    Download a bunch of view module files that use html-template-tag
    library.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const morgan = require("morgan");
+const morgan = require('morgan');
+const PORT = 1234;
 
-app.use(express.static("FOLDER PATH")) // update with folder path
-app.use(morgan("dev"));
-app.use(express.urlencoded({extended: false}));
+app.use(express.static('FOLDER PATH')); // update with folder path
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => { console.log("Listening on --> ", PORT)});
+// app.listen(PORT);
